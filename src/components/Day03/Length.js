46828584d42a610b30176bd0b7cc52346cf140fe
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-03-03 11:37:43 +0000

    Day 03 - Form Challenges: 01 Length

---
+++ src/components/Day03/Length.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+class Length extends Component {
+    constructor() {
+        super();
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            input : ""
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            input : e.currentTarget.value
+        });
+    }
+
+    render() {
+
+        return (
+            <>
+                <br></br>
+                <h1>Length Input</h1>
+                <div className="form-group">
+                    <label>Length: {this.state.input.length}</label>
+                    <input className="form-control" onChange = {this.handleChange} />
+                </div>
+            </>
+            );
+
+    }
+
+}
+
+export default Length;
